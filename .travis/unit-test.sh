b1c03178ce6656e238cbcaef2b4518d0433b9c85
Bernie Pope <bjpope@unimelb.edu.au>
2017-12-18 13:41:32 +1100

    Add unit test script for travis

---
+++ .travis/unit-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+errors=0
+
+TOP_DIR=`pwd`
+
+# Run unit tests
+# src/bionitio-build/bionitio-test > /dev/null 2>&1 || {
+src/bionitio-build/bionitio-test || {
+    echo "'unit test' failed"
+    let errors+=1
+}
+
+[ "$errors" -gt 0 ] && {
+    echo "There were $errors errors found"
+    exit 1
+}
+
+echo "Ok : C++ specific tests"
